df2091726932a2f23ad00184af907a4599c43484
Manjot Singh <ms2295>
2019-03-06 11:45:36 -0500

    Task Theme

---
+++ wordpress/wp-content/themes/BootsTheme/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Bootstrap WordPress Theme</title>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php
+        wp_enqueue_style( 'style189304', get_stylesheet_uri() );
+        ?>
+		</head>
+	<body>
+		
+		<div class = "navbar navbar-inverse navbar-static-top">
+			<div class = "container">
+				
+				<a href = "#" class = "navbar-brand">Bootstrap WordPress Theme</a>
+				
+				<button class = "navbar-toggle" data-toggle = "collapse" data-target = ".navHeaderCollapse">
+					<span class = "icon-bar"></span>
+					<span class = "icon-bar"></span>
+					<span class = "icon-bar"></span>
+				</button>
